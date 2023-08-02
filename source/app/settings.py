21cace311dfd38e8b701b58f0609bb200e931960
GLeb Tarakanov <glebtgd@gmail.com>
2023-08-02 22:50:11 +0600

    Создано приложение api

--- source/app/settings.py
+++ source/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_bootstrap5',
     'webapp',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -78,9 +79,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'rootroot',
+        'NAME': 'survey',
+        'USER': 'gleb',
+        'PASSWORD': '1982',
         'HOST': 'localhost',
         'PORT': '',
     }
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
