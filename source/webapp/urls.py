6dfdc3c27122ac8360b9edd86ef1f981b1bc6c02
GLeb Tarakanov <glebtgd@gmail.com>
2023-08-04 22:35:18 +0600

    Главная страница изменена на страницу просмотра доступных опросов Добавлена возможность удаления опроса с подтвержением

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from webapp.views import IndexView
+from webapp.views import SurveyCreateView, SurveyDetailView, SurveyDeleteView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', SurveyDetailView.as_view(), name='index'),
+    path('survey/add', SurveyCreateView.as_view(), name='survey_create'),
+    path('survey/<int:pk>/delete/', SurveyDeleteView.as_view(), name='survey_delete'),
+    path('survey/<int:pk>/confirm_delete/', SurveyDeleteView.as_view(), name='survey_confirm_delete'),
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views.generic import TemplateView, ListView, DeleteView
+
+from webapp.models import Survey
 
 
 # Create your views here.
 
 
-class IndexView(TemplateView):
+class SurveyCreateView(TemplateView):
     template_name = 'survey_constructor.html'
+
+
+class SurveyDetailView(ListView):
+    template_name = 'survey_list.html'
+
+    context_object_name = 'surveys'
+    model = Survey
+
+
+class SurveyDeleteView(DeleteView):
+    template_name = 'survey_confirm_delete.html'
+    model = Survey
+    success_url = reverse_lazy('index')
+
