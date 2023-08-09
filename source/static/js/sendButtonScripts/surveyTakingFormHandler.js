7feabba2b820e0718a5c254647b27cadb98a5ab2
AN-droid-MP <130159045+AN-droid-MP@users.noreply.github.com>
2023-08-10 03:42:35 +0600

    дописан POST-запрос для сохранения ответов

--- source/static/js/sendButtonScripts/surveyTakingFormHandler.js
+++ source/static/js/sendButtonScripts/surveyTakingFormHandler.js
@@ -1,40 +1,21 @@
-// import {displayErrorMessage, showFloatingErrorMessage} from "../confirmButtonScripts/errorHandling";
-function displayErrorMessage() {
-    const errorMessage = document.getElementById('empty-gaps-errorMessage');
-    errorMessage.classList.add('active');
-
-    setTimeout(function () {
-        errorMessage.classList.remove('active');
-    }, 2000);
-}
-
-function showFloatingErrorMessage() {
-    const errorMessageContainer = document.getElementById('too-few-forms-errorMessage');
-    errorMessageContainer.classList.add('active');
-
-    setTimeout(function () {
-        errorMessageContainer.classList.remove('active');
-    }, 2000);
-}
-
 function getTakenQuestionType(input, textarea) {
 
-    if (input && input.type === 'radio') {
-        return 'SINGLE_CHOICE';
-    } else if (input && input.type === 'checkbox') {
-        return 'MULTIPLE_CHOICE';
-    } else if (textarea) {
-        return 'TEXT_FIELD';
-    } else {
-        return 'SECTION';
-    }
+        if (input && input.type === 'radio') {
+            return 'SINGLE_CHOICE';
+        } else if (input && input.type === 'checkbox') {
+            return 'MULTIPLE_CHOICE';
+        } else if (textarea) {
+            return 'TEXT_FIELD';
+        } else {
+            return 'SECTION';
+        }
 
 }
 
 function getTakenFormData() {
-    const surveyType = document.getElementsById('survey-type');
-    const name = document.getElementById('survey-name').value;
-    const description = document.getElementById('survey-description').value;
+    const surveyType = document.getAttribute('survey-type');
+    const name = surveyType.value;
+    const description = ""; //document.getElementById('survey-description').value;
     const currentDate = new Date();
 
     const day = currentDate.getDate().toString().padStart(2, '0').toString();
@@ -107,57 +88,28 @@ function getTakenFormData() {
     };
 }
 
+// Находим кнопку по классу
+const sendButton = document.querySelector('.send-button-active');
 
-function handleButtonClick(event) {
-    const forms = document.querySelectorAll('#formContainer form');
-    const confirmSaveButton = document.getElementById('answer-submit-button');
-
-    if (forms.length === 0) {
-        event.preventDefault();
-        showFloatingErrorMessage();
-        return;
-    }
-
-    if (confirmSaveButton.classList.contains('send-button-disabled')) {
-        event.preventDefault();
-        displayErrorMessage();
-    } else if (confirmSaveButton.classList.contains('send-button-active')) {
-        confirmSaveButton.addEventListener('click', function () {
-            const jsonData = getFormData();
-            console.log('тест');
-            console.log(jsonData);
-
-            fetch('your-server-endpoint', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(jsonData),
-            })
-                .then(response => response.json())
-                .then(data => {
-                    // Обработка ответа от сервера (если необходимо)
-                    console.log('Форма успешно отправлена на сервер!');
-                })
-                .catch(error => {
-                    // Обработка ошибки (если необходимо)
-                    console.error('Ошибка при отправке формы на сервер:', error);
-                });
-        });
-    }
-}
-
-function setupFormHandlers() {
-    const textareaElements = document.querySelectorAll('#formContainer textarea:not(#surveyDescription)');
-    const inputElements = document.querySelectorAll('#formContainer input[type="text"]');
-    const checkboxes = document.querySelectorAll('#formContainer input[type="checkbox"]');
-
-    textareaElements.forEach(textarea => textarea.addEventListener('input', checkFormFields));
-    inputElements.forEach(input => input.addEventListener('input', checkFormFields));
-    checkboxes.forEach(input => input.addEventListener('change', checkFormFields));
-
-    const confirmSaveButton = document.getElementById('confirmSaveButton');
-    confirmSaveButton.addEventListener('click', handleButtonClick);
-}
-
-export {getTakenFormData, setupFormHandlers};
+// Добавляем обработчик события при клике на кнопку
+sendButton.addEventListener('click', function() {
+    // Вызываем функцию, чтобы получить данные формы
+    const formData = getTakenFormData();
+    console.log(formData);
+    // Отправляем данные на сервер в формате JSON
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/your-server-endpoint', true);
+    xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                // Обработка успешной отправки
+                console.log('Данные успешно отправлены на сервер.');
+            } else {
+                // Обработка ошибки отправки
+                console.error('Произошла ошибка при отправке данных на сервер.');
+            }
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+});
