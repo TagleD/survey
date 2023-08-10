088ca7ffce112f0ad4123cd859dded8208877a8c
GLeb Tarakanov <glebtgd@gmail.com>
2023-08-10 16:42:11 +0600

    МОдели зарегистрированы в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
+from webapp.models import Survey, UserAnswer
+
+
 # Register your models here.
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'json_survey', 'created_at')
+    list_filter = ('id', 'name', 'created_at')
+    search_fields = ('id', 'name', 'description', 'json_survey', 'created_at')
+    fields = ('id', 'name', 'description', 'json_survey', 'created_at')
+
+
+admin.site.register(Survey, SurveyAdmin)
+
+
+class UserAnswerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'json_answer', 'created_at', 'survey_id')
+    list_filter = ('id', 'created_at', 'survey_id')
+    search_fields = ('id', 'created_at', 'survey_id')
+    fields = ('id', 'created_at', 'survey_id')
+
+admin.site.register(UserAnswer, UserAnswerAdmin)
