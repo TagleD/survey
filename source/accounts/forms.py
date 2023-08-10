fdb6475ab7bee0c946ba25d89b677710da1f6c76
GLeb Tarakanov <glebtgd@gmail.com>
2023-08-10 14:53:39 +0600

    Добавлена аутентификация и приложение accounts

---
+++ source/accounts/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        required=True,
+        label='Логин'
+    )
+    password = forms.CharField(
+        required=True,
+        label='Пароль',
+        widget=forms.PasswordInput
+    )
+
